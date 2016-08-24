4d928051b6a80cffe2c111cecf2949cb46e53774
musaomondi <amokem@yahoo.com>
2016-08-24 18:19:45 +0300

    add test for the static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,18 @@
 require 'test_helper'
-
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
-    get root_url
+    get :home
     assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
-
   test "should get about" do
-    get static_pages_about_url
+    get :about
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
-
   test "should get contact" do
-    get static_pages_contact_url
+    get :contact
     assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
-
 end
